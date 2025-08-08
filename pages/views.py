92c0dc1b309facf19005d6dba00b6b2686585a3b
lemorenog <lemorenog@eafit.edu.co>
2025-08-08 00:07:01 -0500

    Upload files

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,10 @@
 from django import forms
-from django.shortcuts import render, redirect
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import TemplateView, ListView
 from django.views import View
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from .models import Product
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -40,14 +41,6 @@ class ContactPageView(TemplateView):
         })
 
         return context
-    
-class Product: 
-    products = [ 
-        {"id":"1", "name":"TV", "description":"Best TV", "price": 1000}, 
-        {"id":"2", "name":"iPhone", "description":"Best iPhone", "price": 1200}, 
-        {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price": 50}, 
-        {"id":"4", "name":"Glasses", "description":"Best Glasses", "price": 100}, 
-    ] 
  
 class ProductIndexView(View): 
     template_name = 'products/index.html' 
@@ -56,39 +49,47 @@ class ProductIndexView(View):
         viewData = {} 
         viewData["title"] = "Products - Online Store" 
         viewData["subtitle"] =  "List of products" 
-        viewData["products"] = Product.products 
+        viewData["products"] = Product.objects.all()  # Fetch all products from the database 
  
-        return render(request, self.template_name, viewData) 
+        return render(request, self.template_name, viewData)
  
 class ProductShowView(View): 
     template_name = 'products/show.html' 
  
- 
-    def get(self, request, id): 
+    def get(self, request, id):
+        # Check if product id is valid 
+        try: 
+            product_id = int(id) 
+            if product_id < 1: 
+                raise ValueError("Product id must be 1 or greater") 
+            product = get_object_or_404(Product, pk=product_id)
+        except (ValueError, IndexError): 
+            # If the product id is not valid, redirect to the home page 
+            return HttpResponseRedirect(reverse('home')) 
+
         viewData = {} 
-        
-        # Validar si el ID es válido
-        try:
-            index = int(id) - 1
-            if index < 0 or index >= len(Product.products):
-                return HttpResponseRedirect(reverse('home'))  # redirige a 'home'
-
-            product = Product.products[index]
-        except (ValueError, TypeError):
-            return HttpResponseRedirect(reverse('home'))  # redirige si id no es entero
-        
-        viewData["title"] = product["name"] + " - Online Store" 
-        viewData["subtitle"] =  product["name"] + " - Product information" 
+        product = get_object_or_404(Product, pk=product_id) 
+        viewData["title"] = product.name + " - Online Store" 
+        viewData["subtitle"] =  product.name + " - Product information" 
         viewData["product"] = product 
- 
-        return render(request, self.template_name, viewData)
+        
+        return render(request, self.template_name, viewData) 
 
+class ProductListView(ListView): 
+    model = Product 
+    template_name = 'product_list.html' 
+    context_object_name = 'products'  # This will allow you to loop through 'products' in your template 
 
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs) 
+        context['title'] = 'Products - Online Store' 
+        context['subtitle'] = 'List of products' 
+        return context 
  
-class ProductForm(forms.Form):
-    name = forms.CharField(label="Name", max_length=100)
-    # description = forms.CharField(label="Description", max_length=500, widget=forms.Textarea)
-    price = forms.FloatField(label="Price")
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'price']
 
     def clean_price(self):
         price = self.cleaned_data.get('price')
@@ -101,7 +102,7 @@ class ProductCreateView(View):
     template_name = 'products/create.html' 
  
     def get(self, request): 
-        form = ProductForm() 
+        form = ProductForm()
         viewData = {} 
         viewData["title"] = "Create product" 
         viewData["form"] = form 
@@ -109,11 +110,53 @@ class ProductCreateView(View):
  
     def post(self, request): 
         form = ProductForm(request.POST) 
+
         if form.is_valid(): 
-             
-            return render(request, 'products/created.html')  # Redirect to a success page or render a success template
-        else: 
+            form.save()
+            return redirect('created') # Redirect to a success page or product list after creation
+        else:   
             viewData = {} 
             viewData["title"] = "Create product" 
             viewData["form"] = form 
             return render(request, self.template_name, viewData)
+        
+class CartView(View):
+    template_name = 'cart/index.html'
+
+    def get(self, request):
+        # Simulated database for products
+        products = {}
+        products[121] = {'name': 'Tv samsung', 'price': '1000'}
+        products[11] = {'name': 'Iphone', 'price': '2000'}
+        
+        # Get cart products from session
+        cart_products = {}
+        cart_product_data = request.session.get('cart_product_data', {})
+        # Filter products based on cart data
+        for key, product in products.items():
+            if str(key) in cart_product_data.keys():
+                cart_products[key] = product
+        
+        # Prepare data for the view
+        view_data = {
+            'title': 'Cart - Online Store',
+            'subtitle': 'Shopping Cart',
+            'products': products,
+            'cart_products': cart_products
+        }
+        return render(request, self.template_name, view_data)
+    
+    def post(self, request, product_id):
+        # Get cart products from session and add the new product
+        cart_product_data = request.session.get('cart_product_data', {})
+        cart_product_data[product_id] = product_id
+        request.session['cart_product_data'] = cart_product_data
+        return redirect('cart_index')
+
+class CartRemoveAllView(View):
+    def post(self, request):
+        # Remove all products from cart in session
+        if 'cart_product_data' in request.session:
+            del request.session['cart_product_data']
+
+        return redirect('cart_index')
