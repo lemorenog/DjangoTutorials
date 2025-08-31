81a64628ac49b417ac173beef7666f84fcb19c2e
lemorenog <lemorenog@eafit.edu.co>
2025-08-30 22:56:54 -0500

    New tutorial

--- helloworld_project/settings.py
+++ helloworld_project/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',  # new
+
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+IMAGE_STORAGE_CLASS = 'pages.utils.ImageLocalStorage'
--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView, CartView, CartRemoveAllView
+
+from pages.utils import ImageLocalStorage
+from .views import HomePageView, AboutPageView, ContactPageView, ImageViewFactory, ProductIndexView, ProductShowView, ProductCreateView, CartView, CartRemoveAllView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),  # new 
@@ -12,4 +14,6 @@ urlpatterns = [
     path('cart/', CartView.as_view(), name='cart_index'),
     path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
     path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'), 
+    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'),
     ]
